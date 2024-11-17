9c044a1ad8655c77ce570f610f6c36e1749d0791
Ilia Slepnev <islepnev@jinr.ru>
2024-11-17 16:11:51 +0300

    cleanup

--- app/auth/keycloak.py
+++ app/auth/keycloak.py
@@ -1,38 +1,10 @@
 import logging
-from keycloak import KeycloakOpenID
-from app.utils.config_manager import ConfigManager
 from app.context_manager import app_context
 
 logger = logging.getLogger(__name__)
 
 def get_keycloak_openid():
     return app_context.keycloak_openid
-
-# # Lazy initialization of KeycloakOpenID
-# _keycloak_openid = None
-
-
-# def get_keycloak_openid() -> KeycloakOpenID:
-#     """
-#     Lazily initialize and return the KeycloakOpenID instance.
-#     Ensures configuration is loaded before initialization.
-#     """
-#     global _keycloak_openid
-#     if _keycloak_openid is None:
-#         config = ConfigManager.get_config()
-#         try:
-#             _keycloak_openid = KeycloakOpenID(
-#                 server_url=config.keycloak.server_url,
-#                 realm_name=config.keycloak.realm,
-#                 client_id=config.keycloak.client_id,
-#                 client_secret_key=config.keycloak.client_secret,
-#                 verify=config.keycloak.verify_ssl,
-#             )
-#             logger.info("KeycloakOpenID initialized successfully.")
-#         except Exception as e:
-#             logger.error(f"Failed to initialize KeycloakOpenID: {e}")
-#             raise
-#     return _keycloak_openid
 
 
 async def get_user_info(access_token: str):
